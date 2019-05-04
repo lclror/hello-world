a07bb69d18c194b2080ec6328e815231be2c7141
lclror <l95818416@hotmail.com>
2019-05-04 23:04:20 +0800

    fixed step05

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 function foo(source) {
-	const arr = ['master01', 'master02', 'master03-github-fixed', 'master04']
+	const arr = ['master01', 'master02', 'master03-github-fixed', 'master04', 'master05']
 	if(arr.include(source)) return true
 }
