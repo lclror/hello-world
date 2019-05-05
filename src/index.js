777b3dab5d4e3d96ca60c84910af9e4ebd37685f
lclror <l95818416@hotmail.com>
2019-05-05 23:30:36 +0800

    add master06 local

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 function foo(source) {
-	const arr = ['master01', 'master02', 'master03-github-fixed', 'master04', 'master05-github-fixed']
+	const arr = ['master01', 'master02', 'master03-github-fixed', 'master04', 'master05-github-fixed', 'master06-local-fixed']
 	if(arr.include(source)) return true
 }
